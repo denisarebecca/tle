a1028d263b757b7935d73d9933609662c9c99004
denisarebecca <77790645+denisarebecca@users.noreply.github.com>
2021-02-27 00:08:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>
-      Homepage by Food Coders
+      Homepage by Birds of Hope
     </title>
     </head>
 <body background="blu.jpg" link="#000" alink="#017bf5" vlink="#000">
   <br />
   <h3 align="center">
-    <font face="Castellar" size="5">FOOD CODERS</font>
+    <font face="Castellar" size="5">Birds of Hope</font>
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -17,53 +17,86 @@
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <font face="Copperplate gothic bold" size="4" color="white">
-        <a href="#page">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;
+    <font face="Copperplate gothic bold" size="4" color="black">
+        <a href="#page">TODAY</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#tom">TOMORROW</a>&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="#video">VIDEOS</a>&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="#about">ABOUT US</a>&nbsp;&nbsp;&nbsp;&nbsp;
        </font>
   </h3>
   <br /><br /><br /><br /><br /><br /><br /><br />
   <h1 align="center">
-  <font face="Castellar" color="black" size="10">
-     Covid-19
-  </font>
+  <font face="Castellar" color="black" size="45">Covid-19</font>
   <br />
   <h3 align="center">
-  BY THE FOOD CODERS
+  By the Birds of Hope
   </h3>
 </h1>
 <br><p align = "center"><img src = "lol.jpg" Width = "820"  Length = "440">
-  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <p id="page">HOME</p>
+  <br /><br /><br /><br /><br /><br /><br />
+<p align = "center"><Font size = "6" ><font face= "Copperplate gothic bold"><Font color = "black"><b>
+“Hope is the thing with feathers that perches in the soul  
+<br>- and sings the tunes without the words - and never stops at all.” 
+<br>- Emily Dickinson
+</font></b>
+  <br /><br /><br /><br /><br /><br /><br />
+<H2>
+  <p id="page">TODAY</p>
+</H2>
 <br><p align = "left"><Font size = "4"><Font color = "black"><body style = background-color:#FCF8EC>
-What is Covid 19?
-<p align = "left"><Font size = "3"><Font color = "black"> <Font face = "Castellar">
-Coronavirus disease (COVID-19),<br>
+<font size="5"><b>What is Covid 19?</font></b>
+<p align = "left"><Font size = "5"><Font color = "black"> 
+Coronavirus disease (COVID-19),
 <br>is an infectious disease caused by a newly discovered coronavirus.
 <br>Most people who fall sick with COVID-19 will experience mild to moderate symptoms and  
 <br>recover without special treatment. In early 2020, following an outbreak in China in 
 <br>December 2019, the World Health Organization described SARS-CoV-2 as a new form of coronavirus.
-<br>
 <br>The epidemic spread rapidly across the world. COVID-19 is a
 SARS-CoV-2disease that can cause what doctors call respiratory tract infection.
-<br>
 <br>It can affect your upper respiratory tract (sinuses, nose, and throat) or lower respiratory tract (windpipe and lungs). 
-It spreads in the same way as other coronaviruses do, mainly through person-to-person contact. 
+<br>It spreads in the same way as other coronaviruses do, mainly through person-to-person contact. 
 Infections range from mild to fatal.
-  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <p id="video">VIDEOS</p>
-<br><p align = "left"><Font size = "4"><Font color = "black"><body style = background-color:#FCF8EC>
+  <br /><br /><br /><br /><br /><br /><br />
+<H4>  
+<p id="tom">TOMORROW</p>
+</H4>
+<br><p align = "center"><img src = "Birds of Hope.jpg" Width = "766"  Length = "1202">
+<br><p align = "left"><Font size = "5"><Font color = "black">
+<br>Our drawing is all about the star, jesus Christ and the dove. These three things that we have drawn are all we need when it 
+<br>comes to survivng these days. The dove signifies hope itself throughthe form of the holy spirit. While the star serves as
+<br>the light for the three kings. The light that can let them find their way to the son of God and the savior Jesus Christ.
+<br>Jesus there is the one guiding us all throughout these hard times. Now that we encounter a lot of challenges in life, we hold on to prayer.
+<br>A prayer that can be beneficial to ourselves or to everyone else. The three things mentioned above are hope itself. We pray to them that they 
+<br>should guid us  and let us overcome these obstacle in life.
+<br>
+<a href = "https://youtu.be/-16yOdgAIKc">Our Message of Hope</a>
+
+  <br /><br /><br /><br /><br /><br /><br />
+<H3>  
+<p id="video">VIDEOS</p>
+<br><p align = "left"><Font size = "5"><Font color = "black"><body style = background-color:#FCF8EC>
 Here are videos to explain the Covid 19 
 <br><br><br>
 <a href = "https://youtu.be/U8r3oTVMtQ0">Recognizing Day to Day Signs and Symptoms of Coronavirus</a>
 <br><br><br>
 <a href = "https://youtu.be/BtN-goy9VOY">The Coronavirus Explained & What You Should Do</a>
 <br><br><br>
+<a href = "https://youtu.be/h8NSmRFur-g">COVID-19: Philippine schools struggle to educate poor children</a>
+<br><br><br>
 <a href = "https://youtu.be/55ErDdtT2zs">COVID-19: Tracing The First Month Of The Novel Coronavirus | Insight | Full Episode</a>
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-  <p id="about">ABOUT US</p>
+</H3>
+<br /><br /><br /><br /><br /><br /><br /><br />
+<H3> 
+ <p id="about">ABOUT US</p>
+</H3>
+We named ourselves the Birds of Hope because we want to give people hope by giving them information about COVID19, 
+<br>we want to make sure that they shouldn't worry because COVID19 isn't invincible. 
+<br>We don't want to turn everyone down so we provide information like these to make them feel at ease, 
+<br>and to think for themselves that they should get ready since they now know how to avoid COVID19 and what are the symptoms and 
+<br>the like. Our team wants you to rely on us if it's about information, and we also want you to know that we are your symbols of Hope.
+<br>
+Our group consists of five people, Jeremiah Notarte also known as Sparrow, Jude Calderon also known as Depleted Pigeon. 
+<br>Rebecca Denisa also known as Monsieur Chikin Wing, and John Danielle Giducos who is also known as Koreanabulalo. 
+<br>Lastly, Cassandra Tiangson, no contribution.
 </body>
 </html>
