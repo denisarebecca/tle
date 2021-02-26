509d57d22312ddd15326fa1f2360524d6a6515bf
denisarebecca <77790645+denisarebecca@users.noreply.github.com>
2021-02-26 20:12:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,69 @@
 <!DOCTYPE html>
+<html lang="en">
   <head>
+    <meta charset="utf-8">
     <title>
       Homepage by Food Coders
     </title>
     </head>
-<body background= 
+<body background="blu.jpg" link="#000" alink="#017bf5" vlink="#000">
+  <br />
+  <h3 align="center">
+    <font face="Castellar" size="5">FOOD CODERS</font>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <font face="Copperplate gothic bold" size="4" color="white">
+        <a href="#page">HOME</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#video">VIDEOS</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#about">ABOUT US</a>&nbsp;&nbsp;&nbsp;&nbsp;
+       </font>
+  </h3>
+  <br /><br /><br /><br /><br /><br /><br /><br />
+  <h1 align="center">
+  <font face="Castellar" color="black" size="10">
+     Covid-19
+  </font>
+  <br />
+  <h3 align="center">
+  BY THE FOOD CODERS
+  </h3>
+</h1>
+<br><p align = "center"><img src = "lol.jpg" Width = "820"  Length = "440">
+  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+  <p id="page">HOME</p>
+<br><p align = "left"><Font size = "4"><Font color = "black"><body style = background-color:#FCF8EC>
+What is Covid 19?
+<p align = "left"><Font size = "3"><Font color = "black"> <Font face = "Castellar">
+Coronavirus disease (COVID-19),<br>
+<br>is an infectious disease caused by a newly discovered coronavirus.
+<br>Most people who fall sick with COVID-19 will experience mild to moderate symptoms and  
+<br>recover without special treatment. In early 2020, following an outbreak in China in 
+<br>December 2019, the World Health Organization described SARS-CoV-2 as a new form of coronavirus.
+<br>
+<br>The epidemic spread rapidly across the world. COVID-19 is a
+SARS-CoV-2disease that can cause what doctors call respiratory tract infection.
+<br>
+<br>It can affect your upper respiratory tract (sinuses, nose, and throat) or lower respiratory tract (windpipe and lungs). 
+It spreads in the same way as other coronaviruses do, mainly through person-to-person contact. 
+Infections range from mild to fatal.
+  <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+  <p id="video">VIDEOS</p>
+<br><p align = "left"><Font size = "4"><Font color = "black"><body style = background-color:#FCF8EC>
+Here are videos to explain the Covid 19 
+<br><br><br>
+<a href = "https://youtu.be/U8r3oTVMtQ0">Recognizing Day to Day Signs and Symptoms of Coronavirus</a>
+<br><br><br>
+<a href = "https://youtu.be/BtN-goy9VOY">The Coronavirus Explained & What You Should Do</a>
+<br><br><br>
+<a href = "https://youtu.be/55ErDdtT2zs">COVID-19: Tracing The First Month Of The Novel Coronavirus | Insight | Full Episode</a>
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+  <p id="about">ABOUT US</p>
+</body>
+</html>
